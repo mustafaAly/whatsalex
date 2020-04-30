08cb1dd6b49bb41eb5a995fbf01a4a6235bedb03
mustafa aly <mustafaa.aly@outlook.com>
2020-04-30 21:57:34 +0200

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,29 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
+  //"port": 3000,
+  //"mongoUrl": "mongodb+srv://taskapp:mostafa155ali2@cluster0-cplxz.mongodb.net/test?retryWrites=true",
   "port": process.env.PORT,
+  //"mongoUrl": "mongodb://localhost:27017/chat-api",
+
   "mongoUrl": process.env.MONGODB_URI,
   "bodyLimit": "100kb"
 }
+
+
+
+
+
+
+
+// import mongodb from 'mongodb';
+
+// export default {
+//   "port": 3005,
+//   //"mongoUrl": "mongodb+srv://taskapp:mostafa155ali2@cluster0-cplxz.mongodb.net/whatsalex?retryWrites=true&w=majority",
+//   "mongoUrl": "mongodb://localhost:27017/chat-api",
+//   //"port": process.env.PORT,
+//   //"mongoUrl": process.env.MONGODB_URI,
+//   "bodyLimit": "100kb"
+// }
+
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import socket from 'socket.io';
 import Message from './model/message';
 import Channel from './model/channel';
 
-const LocalStrategy  = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 import config from './config';
 import routes from './routes';
@@ -45,17 +45,17 @@ app.get('/', (req, res) => {
 //Listen for connection
 var typingUsers = {};
 
-io.on('connection', function(client) {
+io.on('connection', function (client) {
   console.log('a user connected');
   //Listens for a new chat message
-  client.on('newChannel', function(name, description) {
+  client.on('newChannel', function (name, description) {
     //Create channel
     let newChannel = new Channel({
-    name: name,
-    description: description,
-  });
+      name: name,
+      description: description,
+    });
     //Save it to database
-    newChannel.save(function(err, channel){
+    newChannel.save(function (err, channel) {
       //Send message to those connected in the room
       console.log('new channel created');
       io.emit("channelCreated", channel.name, channel.description, channel.id);
@@ -63,43 +63,43 @@ io.on('connection', function(client) {
   });
 
   //Listens for user typing.
-  client.on("startType", function(userName, channelId){
+  client.on("startType", function (userName, channelId) {
     console.log("User " + userName + " is writing a message...");
     typingUsers[userName] = channelId;
     io.emit("userTypingUpdate", typingUsers, channelId);
   });
 
-  client.on("stopType", function(userName){
+  client.on("stopType", function (userName) {
     console.log("User " + userName + " has stopped writing a message...");
     delete typingUsers[userName];
     io.emit("userTypingUpdate", typingUsers);
   });
 
   //Listens for a new chat message
-  client.on('newMessage', function(messageBody, userId, channelId, userName, userAvatar, userAvatarColor) {
+  client.on('newMessage', function (messageBody, userId, channelId, userName, userAvatar, userAvatarColor) {
     //Create message
 
     console.log(messageBody);
 
     let newMessage = new Message({
-    messageBody: messageBody,
-    userId: userId,
-    channelId: channelId,
-    userName: userName,
-    userAvatar: userAvatar,
-    userAvatarColor: userAvatarColor
-  });
+      messageBody: messageBody,
+      userId: userId,
+      channelId: channelId,
+      userName: userName,
+      userAvatar: userAvatar,
+      userAvatarColor: userAvatarColor
+    });
     //Save it to database
-    newMessage.save(function(err, msg){
+    newMessage.save(function (err, msg) {
       //Send message to those connected in the room
       console.log('new message sent');
 
-      io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
+      io.emit("messageCreated", msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
     });
   });
 });
 /*||||||||||||||||||||END SOCKETS||||||||||||||||||*/
-
+console.log(config.port,"DDDDDDDDDDDDDDDDDDDDDDD")
 app.server.listen(config.port);
 console.log(`Started on port ${app.server.address().port}`);
 
